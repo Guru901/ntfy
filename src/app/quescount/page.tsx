ccb91f462760801508654cd0e65d1dcd992deadc
Gurvinder Singh <133991448+Guru901@users.noreply.github.com>
2024-07-26 16:44:57 +0530

    btn ui fixed

--- src/app/quescount/page.tsx
+++ src/app/quescount/page.tsx
@@ -124,9 +124,9 @@ export default function QuesCount() {
             </SelectContent>
           </Select>
           {loading ? (
-            <Button className="flex justify-center" disabled>
-              <Loader2 className="h-4 w-4 animate-spin" />
-              Submit
+            <Button disabled>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please Wait
             </Button>
           ) : (
             <Button className="h-[38px]" onClick={submitForm}>
